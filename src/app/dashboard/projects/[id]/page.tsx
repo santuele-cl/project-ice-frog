2d6f8bc387b99d96b3016880d188ef3818c0f8b3
Pap1sss <161422556+Pap1sss@users.noreply.github.com>
2024-04-22 17:03:38 +0800

    feat: add fields on location under project form

--- src/app/dashboard/projects/[id]/page.tsx
+++ src/app/dashboard/projects/[id]/page.tsx
@@ -5,9 +5,11 @@ import {
 import { Paper, Stack, Typography } from "@mui/material";
 import ProjectSchedulesTableHeader from "./_components/ProjectSchedulesTableHeader";
 import ProjectSchedulesTableBody from "./_components/ProjectSchedulesTableBody";
+import dayjs from "dayjs";
 
 import ErrorComponent from "@/app/_ui/ErrorComponent";
 import AddProjectSchedulesFormModal from "./_components/AddProjectSchedulesFormModal";
+import ProjectDetails from "../_components/ProjectDetails";
 
 export default async function ProjectId({
   params: { id },
@@ -35,26 +37,33 @@ export default async function ProjectId({
       </Paper>
     );
 
-  const {
-    id: projectId,
-    jobOrder,
-    location,
-    name,
-    notes,
-    createdAt,
-    updatedAt,
-  } = project.data;
 
   return (
     <Stack sx={{ gap: 2 }}>
-      <Typography variant="h4">{`${jobOrder}-99-JobOrder`}</Typography>
       <Paper elevation={1} sx={{ p: 2 }}>
-        <ProjectSchedulesTableHeader>
-          <Typography>{name}</Typography>
-          <Typography>{jobOrder}</Typography>
-          <Typography>{location}</Typography>
-        </ProjectSchedulesTableHeader>
+        {project.data && <ProjectDetails details={project.data}/>}
+        {/* <Typography variant="h4">{`Job Order No. - ${jobOrder}`}</Typography>
+
+        <Paper elevation={1} sx={{ p: 2 }}>
+          
+            <Typography>Project Name: {name} </Typography>
+            <Typography>{`Location: ${building} ${street} ${barangay}, ${city}`}</Typography>
+            <Typography>
+              Start Date: {dayjs(startDate).format("MMM DD, YYYY hh:mm a")}
+            </Typography>
+            <Typography>
+              End Date: {dayjs(endDate).format("MMM DD, YYYY hh:mm a")}
+            </Typography>
+            <Typography>
+              Date Created: {dayjs(createdAt).format("MMM DD, YYYY hh:mm a")}
+            </Typography>
+            <Typography>
+              Date Updated: {dayjs(updatedAt).format("MMM DD, YYYY hh:mm a")}
+            </Typography>
+
+        </Paper> */}
       </Paper>
+
       <Paper elevation={1} sx={{ p: 2 }}>
         <ProjectSchedulesTableBody
           employeeIds={employeeIds.data}
