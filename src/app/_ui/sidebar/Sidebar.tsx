5d210259ad037aae92fee7d10140c85324b70a8c
santuel-cl <santuel.cl@gmail.com>
2024-04-29 17:01:16 +0800

    feat: new sidebar

---
+++ src/app/_ui/sidebar/Sidebar.tsx
@@ -0,0 +1,58 @@
+"use client";
+import DomainIcon from "@mui/icons-material/Domain";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import BadgeIcon from "@mui/icons-material/Badge";
+import { Fragment } from "react";
+import {
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { useRouter } from "next/navigation";
+
+const links = [
+  {
+    label: "Schedules",
+    path: "/dashboard/schedules",
+    icon: () => <CalendarMonthIcon />,
+  },
+  {
+    label: "Projects",
+    path: "/dashboard/projects",
+    icon: () => <AssignmentIcon />,
+  },
+];
+
+export default function Sidebar({ open }: { open: boolean }) {
+  const { push } = useRouter();
+
+  return (
+    <Fragment>
+      {links.map(({ icon, label, path }) => (
+        <ListItem key={path} disablePadding sx={{ display: "block" }}>
+          <ListItemButton
+            sx={{
+              minHeight: 48,
+              justifyContent: open ? "initial" : "center",
+              px: 2.5,
+            }}
+            onClick={() => push(path)}
+          >
+            <ListItemIcon
+              sx={{
+                minWidth: 0,
+                mr: open ? 3 : "auto",
+                justifyContent: "center",
+              }}
+            >
+              {icon()}
+            </ListItemIcon>
+            <ListItemText primary={label} sx={{ opacity: open ? 1 : 0 }} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </Fragment>
+  );
+}
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,78 +1,196 @@
 "use client";
-import Sidebar from "../_ui/dashboard/Sidebar";
-import Navbar from "../_ui/dashboard/Navbar";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 import {
-  Box,
-  Collapse,
-  Paper,
+  Avatar,
+  Button,
+  IconButton,
   Stack,
-  useMediaQuery,
-  useTheme,
+  Theme,
+  styled,
 } from "@mui/material";
-import TemporaryDrawer from "../_ui/dashboard/TemporaryDrawer";
-import { useEffect, useState } from "react";
+import MuiDrawer from "@mui/material/Drawer";
+import { CSSObject } from "@emotion/react";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MenuOpenOutlinedIcon from "@mui/icons-material/MenuOpenOutlined";
+import AdminSidebar from "../_ui/sidebar/AdminSidebar";
+import { useSession } from "next-auth/react";
+import { logout } from "@/actions/auth";
+import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined";
+import Sidebar from "../_ui/sidebar/Sidebar";
+
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const session = useSession();
+  const [open, setOpen] = React.useState(false);
 
-  const [showTemporarySidebar, setShowTemporarySidebar] = useState(false);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
-  useEffect(() => {
-    setShowTemporarySidebar(false);
-  }, [isSmallScreen]);
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
+  console.log(session);
   return (
-    <Box position="relative">
-      <Navbar setShowTemporarySidebar={setShowTemporarySidebar} />
-      <TemporaryDrawer
-        isSmallScreen={isSmallScreen}
-        showTemporarySidebar={showTemporarySidebar}
-        setShowTemporarySidebar={setShowTemporarySidebar}
-      />
-      <Stack
-        direction="row"
-        position="relative"
-        height="calc(100vh - 70px)"
-        bgcolor="background.paper"
+    <Box sx={{ display: "flex" }}>
+      <AppBar
+        position="fixed"
+        sx={{
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+          bgcolor: "white",
+          color: "common.black",
+        }}
+        elevation={0}
       >
-        <Box>
-          <Stack
-            direction="row"
-            height="100%"
-            sx={{ overflowY: "auto", overflowX: "hidden" }}
-            bgcolor="general"
-            borderRight="1px solid rgba(0,0,0,.1)"
+        <Toolbar>
+          <IconButton
+            onClick={() => setOpen((prev) => !prev)}
+            edge="start"
+            sx={{
+              marginRight: 2,
+            }}
           >
-            <Collapse
-              in={!isSmallScreen}
-              orientation="horizontal"
-              timeout={500}
+            {open ? <MenuOpenOutlinedIcon /> : <MenuOutlinedIcon />}
+          </IconButton>
+
+          <Typography variant="h6" noWrap component="div">
+            Synx
+          </Typography>
+          <Stack
+            sx={{
+              ml: "auto",
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 1,
+            }}
+          >
+            <Typography variant="subtitle1" noWrap component="div">
+              {session.data?.user.email}
+            </Typography>
+            <Avatar
+              sx={{
+                bgcolor: "secondary.main",
+                width: "1.5rem",
+                height: "1.5rem",
+              }}
             >
-              <Sidebar />
-            </Collapse>
+              {session?.data?.user.name && session?.data?.user.name[0]}
+            </Avatar>
+            <IconButton
+              onClick={() => logout()}
+              size="small"
+              sx={{ color: "gray.main" }}
+            >
+              <ExitToAppOutlinedIcon fontSize="large" />
+            </IconButton>
           </Stack>
-        </Box>
-        <Box
-          sx={{
-            flexGrow: "1",
-            width: {
-              xs: "100%",
-              sm: "calc(100vw - 250px)",
-            },
-            p: 2,
-            bgcolor: "gray.light",
-            overflowX: "hidden",
-            overflowY: "auto",
-          }}
-        >
-          {children}
-        </Box>
-      </Stack>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader />
+        <Divider />
+        {session.data?.user.role !== "ADMIN" ? (
+          <AdminSidebar open={open} />
+        ) : (
+          <Sidebar open={open} />
+        )}
+        <Divider />
+        <List>
+          {["Account Settings"].map((text, index) => (
+            <ListItem key={text} disablePadding sx={{ display: "block" }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? "initial" : "center",
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : "auto",
+                    justifyContent: "center",
+                  }}
+                >
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 2, overflowX: "auto" }}>
+        <DrawerHeader />
+
+        {children}
+      </Box>
     </Box>
   );
 }
