a9e473197271f8dedc394dd0c5bc43517bf7c82e
santuel-cl <santuel.cl@gmail.com>
2024-05-02 10:21:27 +0800

    feat: middleware rbac

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,11 @@ import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import {
   DEFAULT_LOGIN_REDIRECT,
-  authRoutes,
-  apiRoutePrefix,
-  publicRoutes,
+  AUTH_ROUTES,
+  API_ROUTE_PREFIX,
+  PUBLIC_ROUTES,
+  ADMIN_ROUTES,
+  ERROR_ROUTES,
 } from "./routes";
 import { getToken } from "next-auth/jwt";
 
@@ -12,7 +14,7 @@ const { auth } = NextAuth(authConfig);
 
 export default auth(async (req) => {
   //@ts-expect-error
-  const user = await getToken({
+  const token = await getToken({
     req,
     secret: process.env.AUTH_SECRET!,
     // salt: process.env.AUTH_SECRET!,
@@ -21,15 +23,21 @@ export default auth(async (req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req?.auth;
 
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiRoutePrefix);
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isApiAuthRoute = nextUrl.pathname.startsWith(API_ROUTE_PREFIX);
+  const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname);
+  const isAuthRoute = AUTH_ROUTES.includes(nextUrl.pathname);
+  const isAdminRoute = ADMIN_ROUTES.some((route) =>
+    nextUrl.pathname.startsWith(route)
+  );
 
   if (isApiAuthRoute) return;
 
   if (isAuthRoute) {
-    if (isLoggedIn)
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    if (isLoggedIn) {
+      return Response.redirect(
+        new URL(DEFAULT_LOGIN_REDIRECT[token?.role!], nextUrl)
+      );
+    }
 
     return;
   }
@@ -47,6 +55,13 @@ export default auth(async (req) => {
     // return Response.redirect(
     //   new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
     // );
+  }
+
+  if (isAdminRoute) {
+    if (token?.role !== "ADMIN") {
+      return Response.redirect(new URL(ERROR_ROUTES["not-found"], nextUrl));
+    }
+    return;
   }
 
   return;
