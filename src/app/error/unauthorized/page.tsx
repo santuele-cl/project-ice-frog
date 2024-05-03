ef4f8ea589c78515441f2f86aa371ca65a78cd27
santuel-cl <santuel.cl@gmail.com>
2024-05-03 16:45:15 +0800

    feat: 401 and 404 page

--- src/app/error/unauthorized/page.tsx
+++ src/app/error/unauthorized/page.tsx
@@ -1,3 +1,35 @@
+"use client";
+import { Button, Link, Stack, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
+import NextLink from "next/link";
+
 export default function Unauthorized() {
-  return <div>Unauthorized</div>;
+  const router = useRouter();
+  return (
+    <Stack
+      sx={{
+        placeItems: "center",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        color: "gray.main",
+      }}
+    >
+      <Typography sx={{ fontSize: 150 }}>401</Typography>
+      <Stack
+        sx={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 1,
+          marginTop: "-40px",
+        }}
+      >
+        <Typography sx={{ fontSize: 20 }}>Unauthorized</Typography>
+        <Link component={NextLink} href="/">
+          Home
+        </Link>
+      </Stack>
+    </Stack>
+  );
 }
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,20 +1,36 @@
-import { Paper, Stack, Typography } from "@mui/material";
+"use client";
+import { LinearProgress, Paper, Stack, Typography } from "@mui/material";
 import Logo from "./_ui/Logo";
 
-export default function loading() {
+export default function RootLoadingPage() {
   return (
-    <Paper>
+    <Paper sx={{ bgcolor: "white", height: "100vh", minHeight: "400px" }}>
       <Stack
         sx={{
+          placeItems: "center",
           flexDirection: "row",
           position: "absolute",
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
+          gap: 2,
         }}
       >
-        <Logo />
-        <Typography variant="h5">Synxchronize</Typography>
+        <Logo size={60} />
+        <Stack
+          sx={{
+            placeItems: "center",
+            gap: 2,
+          }}
+        >
+          <Typography sx={{ fontSize: 40, fontWeight: "bold" }}>
+            Synx
+          </Typography>
+          <LinearProgress
+            color="secondary"
+            sx={{ width: 90, height: 6, marginTop: "-18px" }}
+          />
+        </Stack>
       </Stack>
     </Paper>
   );
