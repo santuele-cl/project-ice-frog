b854e2076256b5b5091573d73c96e679770d3e41
santuel-cl <santuel.cl@gmail.com>
2024-05-03 16:59:13 +0800

    fix: set color based on theme

--- src/app/dashboard/projects/[id]/_components/AddProjectSchedulesFormModal.tsx
+++ src/app/dashboard/projects/[id]/_components/AddProjectSchedulesFormModal.tsx
@@ -1,9 +1,9 @@
 "use client";
+import { useState } from "react";
 import { Button, Modal, Stack } from "@mui/material";
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
-import { useState } from "react";
+
 import AddProjectSchedulesForm from "./AddProjectSchedulesForm";
-// import EmployeeScheduleAddForm from "./EmployeeScheduleAddForm";
 
 export default function AddProjectSchedulesFormModal() {
   const [show, setShow] = useState(false);
@@ -11,10 +11,12 @@ export default function AddProjectSchedulesFormModal() {
   return (
     <Stack sx={{ m: 3, minWidth: 100 }} gap={2} direction="row">
       <Button
-        sx={{ width: "100%", bgcolor: "rgba(0,0,255,0.1)", fontSize: 14 }}
+        fullWidth
+        sx={{ bgcolor: "gray.light", fontSize: 14, p: 2 }}
         onClick={() => setShow(true)}
+        startIcon={<AddOutlinedIcon />}
       >
-        <AddOutlinedIcon /> Assign Employee
+        Assign Employee
       </Button>
 
       <Modal open={show} onClose={() => setShow(false)}>
