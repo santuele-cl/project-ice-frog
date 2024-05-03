bef5068211384251488edaa018f1b910aad91a15
remolinmg <161422556+Pap1sss@users.noreply.github.com>
2024-05-03 11:22:43 +0800

    fix: width and color of add employee button

--- src/app/dashboard/projects/[id]/_components/AddProjectSchedulesFormModal.tsx
+++ src/app/dashboard/projects/[id]/_components/AddProjectSchedulesFormModal.tsx
@@ -9,14 +9,13 @@ export default function AddProjectSchedulesFormModal() {
   const [show, setShow] = useState(false);
 
   return (
-    <Stack gap={2} direction="row">
+    <Stack sx={{ m: 3, minWidth: 100 }} gap={2} direction="row">
       <Button
-        variant="contained"
-        startIcon={<AddOutlinedIcon />}
-        onClick={() => setShow(true)}
+        sx={{ width: "100%", bgcolor: "rgba(0,0,255,0.1)", fontSize: 14 }}
       >
-        Assign Employee
+        <AddOutlinedIcon /> Add Employee
       </Button>
+
       <Modal open={show} onClose={() => setShow(false)}>
         <Stack
           sx={{
